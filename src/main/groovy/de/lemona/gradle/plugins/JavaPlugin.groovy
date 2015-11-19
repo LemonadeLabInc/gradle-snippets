38185473f550f37d7f7a77a15b81d3a06a84ffad
Pier Fumagalli <pier@usrz.com>
2015-11-19 18:58:23 +0900

    Wrap only ON "java" plugin.

--- src/main/groovy/de/lemona/gradle/plugins/JavaPlugin.groovy
+++ src/main/groovy/de/lemona/gradle/plugins/JavaPlugin.groovy
@@ -5,28 +5,30 @@ import org.gradle.api.Project
 
 class JavaPlugin implements Plugin<Project> {
   void apply(Project project) {
-    project.configure(project) {
-      // Source and target compatibility
-      sourceCompatibility = '1.7'
-      targetCompatibility = '1.7'
+    project.plugins.withId('java') {
+      project.configure(project) {
+        // Source and target compatibility
+        sourceCompatibility = '1.7'
+        targetCompatibility = '1.7'
 
-      // Add a "provided" configuration
-      configurations {
-        provided
-        provided.extendsFrom(compile)
-      }
+        // Add a "provided" configuration
+        configurations {
+          provided
+          provided.extendsFrom(compile)
+        }
 
-      // Inject the provided configuration in class paths
-      sourceSets {
-        main.compileClasspath += configurations.provided
-        test.compileClasspath += configurations.provided
-        test.runtimeClasspath += configurations.provided
-      }
+        // Inject the provided configuration in class paths
+        sourceSets {
+          main.compileClasspath += configurations.provided
+          test.compileClasspath += configurations.provided
+          test.runtimeClasspath += configurations.provided
+        }
 
-      // Link JavaDOCs properly
-      javadoc {
-        options.links 'https://docs.oracle.com/javase/7/docs/api/'
-        classpath += configurations.provided
+        // Link JavaDOCs properly
+        javadoc {
+          options.links 'https://docs.oracle.com/javase/7/docs/api/'
+          classpath += configurations.provided
+        }
       }
     }
   }
