3519eed484f82946f33c5f99b3f3f3071df768ba
Andrei Ostanin <aostanin@gmail.com>
2019-03-08 12:09:29 +0900

    Add Google repository

--- src/main/groovy/de/lemona/gradle/plugins/GradlePlugin.groovy
+++ src/main/groovy/de/lemona/gradle/plugins/GradlePlugin.groovy
@@ -5,46 +5,51 @@ import org.gradle.api.Project
 
 class GradlePlugin implements Plugin<Project> {
 
-  void apply(Project project) {
-    // Read out our user and project properties
-    Utilities.readUserProperties(project, '.lemonade.properties')
-    Utilities.readProperties(project, 'lemonade.properties')
+    void apply(Project project) {
+        // Read out our user and project properties
+        Utilities.readUserProperties(project, '.lemonade.properties')
+        Utilities.readProperties(project, 'lemonade.properties')
 
-    // Start processing version setup
-    Version.setup(project)
+        // Start processing version setup
+        Version.setup(project)
 
-    // Create our extension
-    project.extensions.create("lemonade", Extension, project)
+        // Create our extension
+        project.extensions.create("lemonade", Extension, project)
 
-    // Configure the project
-    project.configure(project) {
+        // Configure the project
+        project.configure(project) {
 
-      // Trigger actions on our plugins
-      plugins.withId('java')                    { project.apply plugin:'de.lemona.gradle.java'    }
-      plugins.withId('maven-publish')           { project.apply plugin:'de.lemona.gradle.publish' }
+            // Trigger actions on our plugins
+            plugins.withId('java') { project.apply plugin: 'de.lemona.gradle.java' }
+            plugins.withId('maven-publish') { project.apply plugin: 'de.lemona.gradle.publish' }
 
-      plugins.withId('com.android.application') { project.apply plugin:'de.lemona.gradle.android' }
-      plugins.withId('com.android.library')     { project.apply plugin:'de.lemona.gradle.android' }
+            plugins.withId('com.android.application') {
+                project.apply plugin: 'de.lemona.gradle.android'
+            }
+            plugins.withId('com.android.library') {
+                project.apply plugin: 'de.lemona.gradle.android'
+            }
 
-      plugins.withId('com.jfrog.bintray')       { project.apply plugin:'de.lemona.gradle.bintray' }
+            plugins.withId('com.jfrog.bintray') { project.apply plugin: 'de.lemona.gradle.bintray' }
 
-      // S3 plugin gets triggered by property/env variable....
-      if (Utilities.resolveValue(project, 's3.repository', 'S3_REPOSITORY') != null) {
-        project.apply plugin:'de.lemona.gradle.s3'
-      }
+            // S3 plugin gets triggered by property/env variable....
+            if (Utilities.resolveValue(project, 's3.repository', 'S3_REPOSITORY') != null) {
+                project.apply plugin: 'de.lemona.gradle.s3'
+            }
 
-      // Configure the default repositories: ours, jcenter and maven central
-      repositories {
-        maven {
-          name 'lemonade-oss'
-          url 'https://dl.bintray.com/lemonade/maven'
+            // Configure the default repositories: ours, google, jcenter, and maven central
+            repositories {
+                maven {
+                    name 'lemonade-oss'
+                    url 'https://dl.bintray.com/lemonade/maven'
+                }
+                google()
+                jcenter()
+                mavenCentral()
+            }
+
+            // Notify out in the build script
+            logger.lifecycle('Initialized {}:{} version {} (0x{})', group, name, version, Integer.toHexString(version.versionCode))
         }
-        jcenter()
-        mavenCentral()
-      }
-
-      // Notify out in the build script
-      logger.lifecycle('Initialized {}:{} version {} (0x{})', group, name, version, Integer.toHexString(version.versionCode))
     }
-  }
 }
