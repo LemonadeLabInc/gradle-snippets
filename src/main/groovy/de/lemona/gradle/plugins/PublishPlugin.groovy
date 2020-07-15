dab37d227e24dfb0f47d6d2ae53d04fa5efdbd83
tkc <cycleinthecircle@gmail.com>
2020-07-15 13:47:40 +0900

    style: format PublishPlugin

--- src/main/groovy/de/lemona/gradle/plugins/PublishPlugin.groovy
+++ src/main/groovy/de/lemona/gradle/plugins/PublishPlugin.groovy
@@ -7,101 +7,101 @@ import org.gradle.api.tasks.bundling.Jar
 
 class PublishPlugin implements Plugin<Project> {
 
-    void apply(Project project) {
-        // We just trigger our configurations on version/properties
-        project.plugins.withId('maven-publish') {
-            project.configure(project) {
+  void apply(Project project) {
+    // We just trigger our configurations on version/properties
+    project.plugins.withId('maven-publish') {
+      project.configure(project) {
 
-                // Our local (test) maven repository
-                def _repositoryPath = new File(buildDir, 'maven')
+        // Our local (test) maven repository
+        def _repositoryPath = new File(buildDir, 'maven')
 
-                // Inform where we are publishing to
-                logger.info('Publishing artifacts to \"{}\"', _repositoryPath.toURI())
+        // Inform where we are publishing to
+        logger.info('Publishing artifacts to \"{}\"', _repositoryPath.toURI())
 
-                // Inject our repository
-                publishing {
-                    repositories {
-                        maven {
-                            url _repositoryPath
-                        }
-                    }
-                }
-
-                /* ================================================================== */
-                /* JAVA PROJECT PUBLISHING                                            */
-                /* ================================================================== */
-
-                plugins.withId('java') {
-
-                    // Sources JAR for publishing
-                    task([type: Jar], 'publishSourcesJar') {
-                        classifier = 'sources'
-                        from sourceSets.main.allSource
-                    }
-
-                    // JavaDoc JAR for publishing
-                    task([type: Jar, dependsOn: javadoc], 'publishJavadocJar') {
-                        classifier = 'javadoc'
-                        from javadoc.destinationDir
-                    }
-
-                    // Create a "java" maven publication *AFTER* the project has been
-                    // evaluated. If we do this now, dependencies will not be included...
-                    afterEvaluate {
-                        publishing.publications.create('java', MavenPublication) {
-                            from components.java
-                            artifact publishSourcesJar
-                            artifact publishJavadocJar
-                        }
-                    }
-                }
-
-                /* ================================================================== */
-                /* ANDROID LIBRARY PROJECT PUBLISHING                                 */
-                /* ================================================================== */
-
-                plugins.withId('com.android.library') {
-                    afterEvaluate {
-                        publishing {
-                            publications {
-                                def _group = group
-                                def _name = name
-                                def _version = version
-                                release(MavenPublication) {
-                                    from components.release
-
-                                    groupId = _group
-                                    artifactId = _name
-                                    version = _version
-                                }
-                            }
-                        }
-                    }
-                }
-
-                /* ================================================================== */
-                /* ANDROID APPLICATION PROJECT PUBLISHING                             */
-                /* ================================================================== */
-
-                plugins.withId('com.android.application') {
-                    afterEvaluate {
-                        publishing {
-                            publications {
-                                def _group = group
-                                def _name = name
-                                def _version = version
-                                release(MavenPublication) {
-                                    from components.release_apk
-
-                                    groupId = _group
-                                    artifactId = _name
-                                    version = _version
-                                }
-                            }
-                        }
-                    }
-                }
+        // Inject our repository
+        publishing {
+          repositories {
+            maven {
+              url _repositoryPath
             }
+          }
         }
+
+        /* ================================================================== */
+        /* JAVA PROJECT PUBLISHING                                            */
+        /* ================================================================== */
+
+        plugins.withId('java') {
+
+          // Sources JAR for publishing
+          task([type: Jar], 'publishSourcesJar') {
+            classifier = 'sources'
+            from sourceSets.main.allSource
+          }
+
+          // JavaDoc JAR for publishing
+          task([type: Jar, dependsOn: javadoc], 'publishJavadocJar') {
+            classifier = 'javadoc'
+            from javadoc.destinationDir
+          }
+
+          // Create a "java" maven publication *AFTER* the project has been
+          // evaluated. If we do this now, dependencies will not be included...
+          afterEvaluate {
+            publishing.publications.create('java', MavenPublication) {
+              from components.java
+              artifact publishSourcesJar
+              artifact publishJavadocJar
+            }
+          }
+        }
+
+        /* ================================================================== */
+        /* ANDROID LIBRARY PROJECT PUBLISHING                                 */
+        /* ================================================================== */
+
+        plugins.withId('com.android.library') {
+          afterEvaluate {
+            publishing {
+              publications {
+                def _group = group
+                def _name = name
+                def _version = version
+                release(MavenPublication) {
+                  from components.release
+
+                  groupId = _group
+                  artifactId = _name
+                  version = _version
+                }
+              }
+            }
+          }
+        }
+
+        /* ================================================================== */
+        /* ANDROID APPLICATION PROJECT PUBLISHING                             */
+        /* ================================================================== */
+
+        plugins.withId('com.android.application') {
+          afterEvaluate {
+            publishing {
+              publications {
+                def _group = group
+                def _name = name
+                def _version = version
+                release(MavenPublication) {
+                  from components.release_apk
+
+                  groupId = _group
+                  artifactId = _name
+                  version = _version
+                }
+              }
+            }
+          }
+        }
+      }
     }
+  }
 }
